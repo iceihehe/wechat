18ca84f653000cba92da93a944f933a3d8237e1c
jiabing <jiabing@wolongdata.com>
2015-06-15 11:21:12 +0800

    修改了配置文件的位置

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@
 import os
 import sys
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wechat.settings")
 
---
+++ settings/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2015年06月15日 星期一 11时17分16秒
+# File Name: __init__.py
