6f3c7d48661212f1a78c3362c8defa5339f1ca08
iceihehe <iceihehe@gmail.com>
2015-06-15 04:20:18 -0400

    修复bug

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wechat.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 
     from django.core.management import execute_from_command_line
 
