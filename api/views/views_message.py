1bb9fbda5e94a64d3c4b545fa6c8536d0c078d5f
jiabing <jiabing@wolongdata.com>
2015-06-23 09:59:28 +0800

    用于学习抽象类

---
+++ api/views/views_message.py
@@ -0,0 +1,13 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2015年06月23日 星期二 09时45分37秒
+# File Name: views_message.py
+'''
+处理用户输入文字，图片等的数据
+'''
+
+from __future__ import print_function, unicode_literals
+import abc
+
+
+class MessageProcessor(object):
+    __meta__ = abc.ABCMeta
