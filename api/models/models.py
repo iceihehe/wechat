4341f461a5a978638a86d65e1b03e427fc5fec99
jiabing <jiabing@wolongdata.com>
2015-06-23 23:26:05 +0800

    修改models

--- api/models/models.py
+++ api/models/models.py
@@ -1,13 +1,22 @@
 # -*- coding=utf-8 -*-
 
-from mongoengine import Document, StringField, FloatField
+from mongoengine import Document, StringField, FloatField,\
+    EmbededDocumentField, EmbededDocument
+
+
+class Location(EmbededDocument):
+    '''地理位置'''
+    # 经度
+    longitude = FloatField()
+    # 纬度
+    latitude = FloatField()
+    # 精度
+    precision = FloatField()
 
 
 class Followers(Document):
     '''关注的人'''
     # 微信号
     user_id = StringField()
-    # 经度
-    longitude = FloatField()
-    # 纬度
-    latitude = FloatField()
+    # 地理位置
+    location = EmbededDocumentField(Location)
