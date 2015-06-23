810fc9b2b918790551e8585917de44248a1e7be0
jiabing <jiabing@wolongdata.com>
2015-06-23 09:25:52 +0800

    修改了const

--- const.py
+++ const.py
@@ -2,5 +2,5 @@
 # Created Time: 2015年06月15日 星期一 18时32分10秒
 # File Name: const.py
 
-AppID = "wx3e7613edf839fe09"
-AppSecret = "0aaf7e470f1c8abb0f6c4eafab34235e"
+AppID = "wx2102221b928b1121"
+AppSecret = "9606385cb216b2478e098de47e3f869d"
